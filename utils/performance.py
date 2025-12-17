26db466dfc4906bf0e44e40c1b3eb249d2f355f3
byungsoo <byung@bu.edu>
2025-12-17 12:40:42 +0900

    performance

--- utils/performance.py
+++ utils/performance.py
@@ -1,26 +1,25 @@
 import sqlite3, time
 
-conn = sqlite3.connect("performance.database")
+conn = sqlite3.connect("pos.database")
 conn.execute('PRAGMA journal_mode=WAL;')  # Enable WAL mode
 cur = conn.cursor()
 
 start = time.time()
 
-# Insert 1000 records
+# Insert records
 cur.execute("BEGIN")
 for i in range(1000):
     cur.execute("""
-        INSERT INTO Order_Cart (order_status, service_area_id, customer_id, subtotal, charged, special_request)
+        INSERT INTO Order_Cart (order_status, service_area_id, customer_id, subtotal, total, provided_name)
         VALUES (?, ?, ?, ?, ?, ?)""",
         (0, (i % 8) + 1, None, 1000 + i * 10, 1100 + i * 10, f'Test order #{i+1}')
     )
-conn.commit()
+    conn.commit()
 
-# Update 1000 records
-cur.execute("BEGIN")
-cur.execute("UPDATE Order_Cart SET order_status = 11 WHERE order_status = 10")
-conn.commit()
-cur.execute("select * from order_history order by timestamp;")
+# Update records
+    cur.execute("UPDATE Order_Cart SET order_status = 11 WHERE order_status = 10")
+    conn.commit()
+    cur.execute("select * from order_history order by timestamp;")
 
 end = time.time()
 print(f"Total time: {end - start:.4f} seconds")
